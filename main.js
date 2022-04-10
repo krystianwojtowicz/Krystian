731dcf2f58419e8c5973cdd9c906836428e92a84
Krystian Wójtowicz <60481782+krystianwojtowicz@users.noreply.github.com>
2022-04-10 16:03:54 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,83 +1,17 @@
-let btn = document.querySelector(".navbar-toggler");
-let icon = btn.querySelector(".navbar-toggler-icon");
-let container = document.querySelector(".first");
-let nav = document.querySelectorAll('.nav-link3');
-let nav2 = document.querySelector('.nav-link2');
-let contact = document.querySelector('.contact');
-let img = document.getElementById('img');
-let img2 = document.getElementById('img1');
-let img3 = document.getElementById('img2');
+const burgerIcon = document.querySelector('.bookmark__img');
+const nav = document.querySelector('.bookmark');
 
-btn.onclick = function (){
-    if(icon.classList.contains("navbar-toggler-icon")){
-        icon.classList.remove("navbar-toggler-icon");
-        icon.classList.add("fa", "fa-times");
-        container.classList.add('open');
-        img.classList.add('open-logo');
-        img2.classList.add('open-logo');
-        img3.classList.add('open2');
-        contact.classList.add('menu-contact')
-        nav.forEach((e) => {
-            e.classList.add('open-link')
-        })
-        nav2.classList.add('login');
-        nav2.classList.remove('to-remove');
-    }
-    else{
-        contact.classList.remove('menu-contact')
-        img.classList.remove('open-logo');
-        img2.classList.remove('open-logo');
-        img3.classList.remove('open2');
-        container.classList.remove('open');
-        nav.forEach((e) => {
-            e.classList.remove('open-link')
-        })
-        icon.classList.remove("fa", "fa-times");
-        icon.classList.add("navbar-toggler-icon");
-        nav2.classList.remove('login');
-        nav2.classList.add('to-remove');
-    }
-}
 
-// const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
-// // const accordionItems = document.querySelectorAll(".accordion-item");
 
-// accordionItemHeaders.forEach(accordionItemHeader => {
-//   accordionItemHeader.addEventListener("click", event => {
-//     const accordionItem = accordionItemHeader.parentElement;
-//     // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-    
-//     // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-//     // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-//     //   currentlyActiveAccordionItemHeader.classList.toggle("active");
-//     //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-//     // }
-//     const accordionItemBody = accordionItemHeader.nextElementSibling;
-//     // const accordionItemBody = parentNode.thirdChild
-// if (!accordionItem.classList.contains('active')){
-//   accordionItem.className = "accordion-item active";
-//     accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-// }
-//     else {
-//       accordionItem.className = "accordion-item";
-//       accordionItemBody.style.maxHeight = 0;
-//     } 
-    
-    
+burgerIcon.addEventListener('click', function(){
+    nav.classList.toggle('active'); 
+   
+})
 
-//     // const accordionItemBody = accordionItemHeader.nextElementSibling;
-//     // if(accordionItemHeader.classList.contains("active")) {
-//     //   accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-//     // }
-//     // else {
-//     //   accordionItemBody.style.maxHeight = 0;
-//     // }
-    
-//   });
-// });
 
-const accordionItemImgs = document.querySelectorAll(".accordion-item-img");
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+const accordionItemImgs = document.querySelectorAll(".accordion__item-img");
+const accordionItemHeaders = document.querySelectorAll(".accordion__item-header");
 
 accordionItemImgs.forEach(accordionItemImg => {
   accordionItemImg.addEventListener("click", event => {
@@ -85,11 +19,11 @@ accordionItemImgs.forEach(accordionItemImg => {
     const accordionItem = accordionItemHeader.parentElement;
     const accordionItemBody = accordionItemHeader.nextElementSibling;
 if (!accordionItem.classList.contains('active')){
-  accordionItem.className = "accordion-item active";
+  accordionItem.className = "accordion__item active";
     accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
 }
     else {
-      accordionItem.className = "accordion-item";
+      accordionItem.className = "accordion__item";
       accordionItemBody.style.maxHeight = 0;
     } 
     
@@ -101,18 +35,20 @@ accordionItemHeaders.forEach(accordionItemHeader => {
     const accordionItem = accordionItemHeader.parentElement;
     const accordionItemBody = accordionItemHeader.nextElementSibling;
 if (!accordionItem.classList.contains('active')){
-  accordionItem.className = "accordion-item active";
+  accordionItem.className = "accordion__item active";
     accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
 }
     else {
-      accordionItem.className = "accordion-item";
+      accordionItem.className = "accordion__item";
       accordionItemBody.style.maxHeight = 0;
     } 
     
   });
 });
 
-const counter = document.querySelector('.counter');
+
+
+const counter = document.querySelector('.already__counter');
 const speed = 1000; // The lower the slower
 
 	const updateCount = () => {
@@ -143,13 +79,13 @@ const speed = 1000; // The lower the slower
   window.addEventListener("load", function(){
     setTimeout(
         function open(event){
-            document.querySelector(".popup").style.display = "block";
+            document.querySelector(".simple__img").style.display = "block";
         },
         20000
     )
 });
-document.querySelector(".popup").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
+document.querySelector(".simple__img").addEventListener("click", function(){
+    document.querySelector(".simple__img").style.display = "none";
 });
 
 
@@ -183,7 +119,7 @@ const emailValue = document.querySelector("[id='emaill']").value.trim();
     document.querySelector('small').classList.add('error');
     document.querySelector('small').innerText = message;
     const formControl = input.parentElement;
-	formControl.className = 'email error';
+	formControl.className = 'already__email error';
   const submit = formControl.parentElement;
   submit.className = 'submit-error'
   }
